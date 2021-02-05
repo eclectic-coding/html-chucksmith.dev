233651c2cd527455989baf824d870eaf32ab93b7
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-02-05 08:50:16 -0500

    Add gulp processing

---
+++ gulp.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  // Settings: Turn on/off build features
+  clean: true,
+  styles: true,
+  images: true,
+  scripts: true,
+  copy: true,
+  reload: true,
+
+  // Global project folder destinations
+  input: "src/",
+  html: "./",
+  output: "dist/",
+
+  // Styles
+  stylesSrc: "src/sass/**/*.{scss,sass,css}",
+  stylesDest: "dist/css/",
+
+  // Images
+  imagesSrc: "src/img/*.*",
+  imagesDest: "dist/img/",
+
+  // Scripts
+  scriptsSrc: "src/js/*",
+  scriptsDest: "dist/js/",
+
+  // Copy
+  copySrc: "src/copy/**/*",
+  copyDest: "dist/",
+
+  // reload
+  reload: "./dist",
+};
