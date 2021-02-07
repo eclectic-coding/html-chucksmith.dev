2808559ac8fc260605e8c6705609626ab85d7b49
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-02-07 17:02:25 -0500

    Add click event for active state menu clicks

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
-console.log("My main.js")
+const linkContainer = document.getElementById("navbar");
+const links = linkContainer.getElementsByClassName("navbar__link");
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function() {
+    let current = document.getElementsByClassName("navbar__link--active");
+    current[0].className = current[0].className.replace("navbar__link--active", "");
+    this.className += " navbar__link--active";
+  });
+}
